80190e48594cd38e1e0e089e83bf4c46074ddde4
martaprosniak <martaprosniak@gmail.com>
2023-10-21 23:25:20 +0200

    Google icon

--- src/components/auth/google/SignIn.tsx
+++ src/components/auth/google/SignIn.tsx
@@ -1,5 +1,6 @@
 import { auth, provider } from '../../../firebase'
 import { signInWithPopup } from 'firebase/auth'
+import googleIcon from '../../../assets/google-icon.png'
 
 export const SignIn = () => {
 
@@ -9,5 +10,5 @@ export const SignIn = () => {
     }
 
 
-    return <div><button onClick={handleSignIn}>Sign in with Google</button></div>
+    return <div><button onClick={handleSignIn}><img src={googleIcon} alt='Google logo' style={{ width: '20px', height: '20px' }} />Sign in with Google</button></div>
 }
